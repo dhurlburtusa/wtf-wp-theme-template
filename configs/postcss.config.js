eedda53d9fffc15c9d53c82e416a9fc4cc42a9f1
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2019-09-05 22:12:48 -0600

    Update PostCSS Configuration Comments.

--- configs/postcss.config.js
+++ configs/postcss.config.js
@@ -27,6 +27,49 @@ module.exports = (ctx) => {
       // }),
       require('postcss-strip-inline-comments')(),
       require('precss')({
+        // Set the `autoprefixer` options. See
+        // https://github.com/csstools/postcss-preset-env#autoprefixer and
+        // https://github.com/postcss/autoprefixer#options.
+        autoprefixer: {
+          // Whether Autoprefixer should use Visual Cascade, if CSS is uncompressed.
+          // cascade: false,
+          // cascade: true, // The default.
+
+          // Environment for Browserslist. See
+          // https://github.com/browserslist/browserslist#configuring-for-different-environments.
+          // env: undefined, // The default.
+
+          // Whether or how Autoprefixer should add prefixes for `flexbox` properties.
+          // When set to `'no-2009'`, Autoprefixer will add prefixes only for final and
+          // IE 10 versions of the specification.
+          // flexbox: 'no-2009',
+          // flexbox: false,
+          // flexbox: true, // The default.
+
+          // Whether Autoprefixer should add IE 10-11 prefixes for Grid Layout properties.
+          //
+          // - `false`: Prevent Autoprefixer from outputting CSS Grid translations. (default)
+          // - `'autoplace'`: Enable Autoprefixer grid translations and include autoplacement
+          //   support.
+          // - `'no-autoplace'`: Enable Autoprefixer grid translations but exclude
+          //   autoplacement support.
+          // grid: 'autoplace',
+          // grid: 'no-autoplace',
+          // grid: false, // The default.
+
+          // Whether Autoprefixer should remove outdated prefixes.
+          // remove: false,
+          // remove: true, // The default.
+
+          // Whether Autoprefixer should add prefixes for `@supports` parameters.
+          // supports: false,
+          // supports: true, // The default.
+        },
+
+        // If not valid browserslist configuration is specified, the default browserslist
+        // query will be used. See https://github.com/csstools/postcss-preset-env#browsers.
+        // browsers: '> 0.5%, last 2 versions, Firefox ESR, not dead', // The default. See https://github.com/browserslist/browserslist#queries.
+
         // By default, plugin will bubble only `@media` and `@supports` at-rules. See
         // https://github.com/postcss/postcss-nested#bubble.
         // bubble: ['media', 'supports'], // The default.
@@ -50,6 +93,44 @@ module.exports = (ctx) => {
         // By default, this name is `extend`, meaning `@extend` rules are parsed. See
         // https://github.com/csstools/postcss-extend-rule#name.
         // extend: 'extend', // The default.
+
+        // The `features` option enables or disables specific polyfills by ID. See
+        // https://github.com/csstools/postcss-preset-env#features.
+        features: {
+          // 'all-property': false,
+          // 'any-link-pseudo-class': false,
+          // 'blank-pseudo-class': false,
+          // 'break-properties': false,
+          // 'case-insensitive-attributes': false,
+          // 'color-functional-notation': false,
+          // 'color-mod-function': false,
+          // 'custom-media-queries': false,
+          // 'custom-properties': false,
+          // 'custom-selectors': false,
+          // 'dir-pseudo-class': false,
+          // 'double-position-gradients': false,
+          // 'environment-variables': false,
+          // 'focus-visible-pseudo-class': false,
+          // 'focus-within-pseudo-class': false,
+          // 'font-variant-property': false,
+          // 'gap-properties': false,
+          // 'gray-function': false,
+          // 'has-pseudo-class': false,
+          // 'hexadecimal-alpha-notation': false,
+          // 'image-set-function': false,
+          // 'lab-function': false,
+          // 'logical-properties-and-values': false,
+          // 'matches-pseudo-class': false,
+          // 'media-query-ranges': false,
+          // 'nesting-rules': false,
+          // 'not-pseudo-class': false,
+          // 'overflow-property': false,
+          // 'overflow-wrap-property': false,
+          // 'place-properties': false,
+          // 'prefers-color-scheme-query': false,
+          // 'rebeccapurple-color': false,
+          // 'system-ui-font-family': false,
+        },
 
         // When a lookup cannot be resolved, this specifies whether to throw an error or
         // log a warning. In the case of a warning, the invalid lookup value will be
@@ -90,10 +171,29 @@ module.exports = (ctx) => {
         // See https://github.com/csstools/postcss-extend-rule#onunusedextend.
         // onUnusedExtend: 'remove', // The default.
 
+        // The `preserve` option determines whether all plugins should receive a
+        // `preserve` option, which may preserve or remove otherwise polyfilled CSS. By
+        // default, this option is not configured. See
+        // https://github.com/csstools/postcss-preset-env#preserve.
+        // preserve: false,
+        // preserve: true,
+
         // By default, plugin will strip out any empty selector generated by intermediate
         // nesting levels. You can set `preserveEmpty` to true to preserve them. See
         // https://github.com/postcss/postcss-nested#preserveempty.
         // preserveEmpty: false, // The default.
+
+        // The `stage` option determines which CSS features to polyfill, based upon their
+        // stability in the process of becoming implemented web standards. Setting stage to
+        // `false` will disable every polyfill. Doing this would only be useful if you
+        // intended to exclusively use the `features` option. See
+        // https://github.com/csstools/postcss-preset-env#stage.
+        // stage: false,
+        // stage: 0,
+        // stage: 1,
+        // stage: 2, // The default.
+        // stage: 3,
+        // stage: 4,
 
         // The unresolved option defines how unresolved variables, mixins, and imports
         // should be handled.
