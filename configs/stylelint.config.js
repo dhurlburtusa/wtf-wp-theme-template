096875eb693bbb0aa939e4d191666e83495bbed9
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2019-09-07 15:35:50 -0600

    Prevent Unnecessary Vendor Prefixes

--- configs/stylelint.config.js
+++ configs/stylelint.config.js
@@ -23,9 +23,19 @@ const config = {
         ignoreAtRules: [],
       },
     ],
+    // When using Autoprefixer, no need to use prefixes in source.
+    'at-rule-no-vendor-prefix': true,
     'comment-empty-line-before': null,
     'indentation': 'tab',
     'max-empty-lines': 2,
+    // When using Autoprefixer, no need to use prefixes in source.
+    'media-feature-name-no-vendor-prefix': true,
+    // When using Autoprefixer, no need to use prefixes in source.
+    'property-no-vendor-prefix': true,
+    // When using Autoprefixer, no need to use prefixes in source.
+    'selector-no-vendor-prefix': true,
+    // When using Autoprefixer, no need to use prefixes in source.
+    'value-no-vendor-prefix': true,
   },
 };
 
